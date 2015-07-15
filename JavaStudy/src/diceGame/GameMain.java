7d0e6246a71036a0b07fdeb808f724249f5833ff
gim-yuncheol-second <gim-yuncheol-second@192.168.0.31>
2015-07-15 10:59:53 +0900

    요구 사항의 5개의 객체 생성

---
+++ JavaStudy/src/diceGame/GameMain.java
@@ -0,0 +1,20 @@
+
+package diceGame;
+
+public class GameMain {
+
+	Player player1;
+	Player player2;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String playerName1;
+		String playerName2;
+		
+		
+	}
+
+	
+	//사용자가 집어넣은 이름으로 플레이어들을 생성한다.
+
+}
